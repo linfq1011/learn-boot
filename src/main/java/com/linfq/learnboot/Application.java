1bb26f845b8691b1cdb3529048d2270692627f83
林凤强(10019929) <linfq@glodon.com>
2019-08-11 18:40:06 +0800

    引入websocket依赖

--- src/main/java/com/linfq/learnboot/Application.java
+++ src/main/java/com/linfq/learnboot/Application.java
@@ -3,6 +3,12 @@ package com.linfq.learnboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 启动类.
+ *
+ * @author linfq
+ * @date 2019/7/31 14:00
+ */
 @SpringBootApplication
 public class Application {
 
